7e96f2a59cc50322b4193b2db22d39f10025d5ac
Piotr Janik <piotrjanik@nautilia.pl>
2025-07-17 19:58:55 +0200

    fix: add metav1 import for Kubernetes metadata handling

--- internal/controller/user_controller.go
+++ internal/controller/user_controller.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
